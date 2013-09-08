2e9761ac4ea0b60c1931e8cb73e69b32f26e3a80
yawboakye <yawboakye@gmail.com>
2013-09-08 19:07:28 +0000

    gave a proper id to the stuff that keeps doding

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
 (function() { return "use strict"; })();
 
-function(){
+
+
+( function ($) {
 	// Calling the aniimate Div function
+
+	var dodgeball = $('#dodgeball');
+
 	$('#button').click(function(){
 		$('.a').show();
 		animateDiv();
@@ -9,27 +14,29 @@ function(){
 
 	// Computing the scores for every click
 	var sum = 0;
+
 	$('.a').click(function(){
-		sum = sum + 100;
+		sum += 100;
 		$('#user').html('Your Score: '+sum);
 	});
-}
+	
+	// This function returns an array of length 2 that contains a random point in my game-area div
+	function makeNewPosition(){
+		var h = $('#game-area').height() - 25;
+		var w = $('#game-area').width() - 25;
 
-// This function returns an array of length 2 that contains a random point in my game-area div
-function makeNewPosition(){
-	var h = $('#game-area').height() - 25;
-	var w = $('#game-area').width() - 25;
+		var nh = Math.floor(Math.random() * h);
+		var nw = Math.floor(Math.random() * w);
 
-	var nh = Math.floor(Math.random() * h);
-	var nw = Math.floor(Math.random() * w);
+		return [nh, nw];
+	}
 
-	return [nh, nw];
-}
+	// Creating the animate function to animate the div
+	function animateDiv(){
+		var newq = makeNewPosition();
+		$('.a').animate({ top: newq[0], left: newq[1] }, 1000, function(){
+	  	animateDiv();
+		});
+	}
 
-// Creating the animate function to animate the div
-function animateDiv(){
-	var newq = makeNewPosition();
-	$('.a').animate({ top: newq[0], left: newq[1] }, 1000, function(){
-  	animateDiv();        
-	});
-}
+} )(jQuery);
