4973f823ab41fe99871517b7d24bb9a84669338d
yawboakye <yawboakye@gmail.com>
2013-09-08 18:52:45 +0000

    removed document.ready since jQuery is at bottom now

--- game.js
+++ game.js
@@ -1,34 +1,34 @@
 
-$(document).ready(function(){
-		// Calling the aniimate Div function
-		$('#button').click(function(){
-			$('.a').show();
-			animateDiv();
-		});
-
-		// Computing the scores for every click
-		var sum = 0;
-		$('.a').click(function(){
-			sum = sum + 100;
-			$('#user').html('Your Score: '+sum);
-		});
+function(){
+	// Calling the aniimate Div function
+	$('#button').click(function(){
+		$('.a').show();
+		animateDiv();
 	});
 
-	// This function returns an array of length 2 that contains a random point in my game-area div
-	function makeNewPosition(){
-		var h = $('#game-area').height() - 25;
-		var w = $('#game-area').width() - 25;
+	// Computing the scores for every click
+	var sum = 0;
+	$('.a').click(function(){
+		sum = sum + 100;
+		$('#user').html('Your Score: '+sum);
+	});
+}
 
-		var nh = Math.floor(Math.random() * h);
-    	var nw = Math.floor(Math.random() * w);
+// This function returns an array of length 2 that contains a random point in my game-area div
+function makeNewPosition(){
+	var h = $('#game-area').height() - 25;
+	var w = $('#game-area').width() - 25;
 
-    	return [nh, nw];
-	}
+	var nh = Math.floor(Math.random() * h);
+	var nw = Math.floor(Math.random() * w);
 
-	// Creating the animate function to animate the div
-	function animateDiv(){
-		var newq = makeNewPosition();
-    	$('.a').animate({ top: newq[0], left: newq[1] }, 1000, function(){
-      	animateDiv();        
-    	});
-	}
+	return [nh, nw];
+}
+
+// Creating the animate function to animate the div
+function animateDiv(){
+	var newq = makeNewPosition();
+	$('.a').animate({ top: newq[0], left: newq[1] }, 1000, function(){
+  	animateDiv();        
+	});
+}
